f38bec578debd02eabab51cdd15ef066a46cec04
Nicollas Leal <nleal439@gmail.com>
2023-04-20 21:33:47 -0300

    Adicionar arquivos de projeto.

---
+++ CRUD_Departament/Controllers/DepartamentoController.cs
@@ -0,0 +1,71 @@
+﻿using CRUD_Departament.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD_Departament.Controllers
+{
+    public class DepartamentoController : Controller
+    {
+        private static IList<Departamento> departamentos = new List<Departamento>()
+        {
+            new Departamento
+            {
+                Id = 1,
+                Nome = "Bradesco"
+            },
+
+            new Departamento
+            {
+                Id = 2,
+                Nome = "Caixa"
+            }
+        };
+
+        public IActionResult Index()
+        {
+
+            return View(departamentos);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Departamento departamento)
+        {
+            departamento.Id = departamentos.Select(i => i.Id).Max() + 1;
+            departamentos.Add(departamento);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Details(long id)
+        {
+            return View(departamentos.Where(i => i.Id == id).First());
+        }
+        public IActionResult Edit(long id)
+        {
+            return View(departamentos.Where(i => i.Id == id).First());
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Edit(Departamento departamento)
+        {
+            departamentos.Remove(departamentos.Where(i => i.Id == departamento.Id).First());
+            departamentos.Add(departamento);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(long id)
+        {
+            return View(departamentos.Where(i => i.Id == id).First());
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Delete(Departamento departamento)
+        {
+            departamentos.Remove(departamentos.Where(i => i.Id == departamento.Id).First());
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ CRUD_Departament/Models/Departamento.cs
@@ -0,0 +1,8 @@
+﻿namespace CRUD_Departament.Models
+{
+    public class Departamento
+    {
+        public long? Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
